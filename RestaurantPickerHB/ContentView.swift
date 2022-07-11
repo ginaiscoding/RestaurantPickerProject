3208bd12483ff86632d53710e51b50101bc209f6
ginaiscoding <rpaek88@gmail.com>
2022-07-11 10:51:55 -0700

    Initial Commit

---
+++ RestaurantPickerHB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestaurantPickerHB
+//
+//  Created by Regina Paek on 7/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestaurantPickerHB/RestaurantPickerHBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantPickerHBApp.swift
+//  RestaurantPickerHB
+//
+//  Created by Regina Paek on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantPickerHBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
